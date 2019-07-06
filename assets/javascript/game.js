afc6f357d6a1003d3b5c4546b2efb335ef426eaf
Kristina Calumpong <kcalumpong@gmail.com>
2019-07-05 20:10:15 -0700

    += to the crystal values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,16 +10,20 @@ var ninteenToOneTwenty = function() {
 
 $(document).ready(function(){
     
-    var addScore;
-
+    var totalScore = 0;
+    var wins = 0;
+    var losses = 0;
+  
+    
     $("#randomNumber").text(ninteenToOneTwenty);
+    
 
- 
     $("#blueCrystal").on("click", function() {
         console.log("blue");
         var blueCrystalVal = oneToTwelve();
         console.log(oneToTwelve());
-        $("#totalScore").text(blueCrystalVal);
+        totalScore += blueCrystalVal;
+        $("#totalScore").text(totalScore); 
     })
 
     $("#redCrystal").on("click", function() {
@@ -27,6 +31,8 @@ $(document).ready(function(){
         var redCrystalVal = oneToTwelve();
         console.log(oneToTwelve());
         $("#totalScore").text(redCrystalVal);
+        totalScore += redCrystalVal;
+        $("#totalScore").text(totalScore);
     })
 
     $("#yellowCrystal").on("click", function() {
@@ -34,6 +40,8 @@ $(document).ready(function(){
         var yellowCrystalVal = oneToTwelve();
         console.log(oneToTwelve());
         $("#totalScore").text(yellowCrystalVal);
+        totalScore += yellowCrystalVal;
+        $("#totalScore").text(totalScore);
     })
 
     $("#greenCrystal").on("click", function() {
@@ -41,7 +49,11 @@ $(document).ready(function(){
         var greenCrystalVal = oneToTwelve();
         console.log(oneToTwelve());
         $("#totalScore").text(greenCrystalVal);
+        totalScore += greenCrystalVal;
+        $("#totalScore").text(totalScore);
     })
+
+    // if totalScore === 
 
     
  
